cecfddb77ff216e6841d61c3bd4b124ab7783c21
Yoann Fleury <yoann.fleury@yahoo.com>
2022-01-31 18:28:32 +0100

    feat: add route to generate csv

---
+++ src/pages/api/issues/generate.ts
@@ -0,0 +1,19 @@
+import { apiMethods } from '@/utils/api';
+import { db } from '@/utils/db';
+
+export default apiMethods({
+  GET: {
+    isPublic: false,
+    handler: async (req, res) => {
+      const issues = await db.issue.findMany();
+
+      const body = issues.reduce(
+        (accumulator, current) =>
+          accumulator + `${current.title},${current.description}\n`,
+        'title,description\n'
+      );
+
+      res.setHeader('Content-Type', 'text/csv').status(200).send(body);
+    },
+  },
+});
