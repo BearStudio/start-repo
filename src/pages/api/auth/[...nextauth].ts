09e7174e1a714fa982a68cb672fff46acc95b787
Yoann Fleury <yoann.fleury@yahoo.com>
2022-02-01 14:16:56 +0100

    feat: prepare for organisation check

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -10,7 +10,24 @@ export default NextAuth({
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name || profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+          username: profile.login,
+        };
+      },
     }),
   ],
+  callbacks: {
+    async signIn({ account }) {
+      // TODO, get the username from the account, request for organisations,
+      // check if user is from @bearstudio.fr
+      // https://api.github.com/users/{username}/orgs
+      return true;
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
 });
