ff95801e7f1003e4508725bac0bd58828444d450
Yoann Fleury <yoann.fleury@yahoo.com>
2022-04-04 23:01:15 +0200

    feat(#25): add disclaimer message for users at login

--- src/app/auth/LoginForm.tsx
+++ src/app/auth/LoginForm.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-import { Box, Button } from '@chakra-ui/react';
+import { Box, Button, Stack, Text } from '@chakra-ui/react';
 import { signIn } from 'next-auth/react';
 import { useTranslation } from 'react-i18next';
-import { FiGithub } from 'react-icons/fi';
+import { FiGithub, FiInfo } from 'react-icons/fi';
+
+import { Icon } from '@/components';
 
 export const LoginForm = ({ onSuccess = () => undefined, ...rest }: TODO) => {
   const { t } = useTranslation();
 
   return (
-    <Box {...rest}>
+    <Stack {...rest}>
       <Button
         w="full"
         colorScheme="github"
@@ -21,6 +23,16 @@ export const LoginForm = ({ onSuccess = () => undefined, ...rest }: TODO) => {
       >
         {t('auth:login.actions.github.login')}
       </Button>
-    </Box>
+      <Box borderRadius="md" bg="github.100" color="github.800" p="4">
+        <Text>
+          <Icon icon={FiInfo} /> Only users from{' '}
+          <Text as="span" fontWeight="medium">
+            {process.env.NEXT_PUBLIC_GITHUB_ALLOWED_ORGANIZATIONS}
+          </Text>{' '}
+          GitHub organisation(s) will be able to use the application once logged
+          in.
+        </Text>
+      </Box>
+    </Stack>
   );
 };
