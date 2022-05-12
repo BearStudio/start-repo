54e828cb4bd87503b93b62abe23b826e4d0a0f59
Yoann Fleury <yoann.fleury@yahoo.com>
2022-05-12 23:29:59 +0200

    fix: using correct configuration for backend session retrieval

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,16 +1,15 @@
 import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
-// import { getServerSession } from 'next-auth/next';
-import { getSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth/next';
 
-// import { authOptions } from '@/lib/auth';
+import { authOptions } from '@/lib/auth';
 import { db } from '@/utils/db';
 
 export const createContext = async ({
   req,
   res,
 }: trpcNext.CreateNextContextOptions) => {
-  const session = await getSession({ req });
+  const session = await getServerSession({ req, res }, authOptions);
 
   return {
     req,
